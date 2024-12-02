ad83bcfb3b07e3ed39a89e3144206286fa88ae1d
Shnep <101168398+1Alligator@users.noreply.github.com>
2024-12-02 00:28:10 -0600

    Added test suite files
    Some files may still be required or have errors.

---
+++ suiteTest8.cpp
@@ -0,0 +1,8 @@
+
+#include "pch.h"
+#include "C:\VSProjects\Sample-Test1\MyTestSuite\tester.cpp"
+
+TEST(TestCaseName, TestName) {
+	EXPECT_EQ("d8836a0875edea57508758554326957e87585ad96620192877890a9889d1203a0192938377894813655e5368397", tester());
+	EXPECT_TRUE(true);
+}
